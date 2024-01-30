0012dabb9fc30a4f9a2c3f4497bdf4b716417fa2
Mateusz <smykal@engineer.com>
2024-01-30 21:19:27 +0100

    move showBodyParams to different Component

--- frontend/src/components/zapiszto/index.tsx
+++ frontend/src/components/zapiszto/index.tsx
@@ -1,84 +1,26 @@
 import { Component } from "react";
 
-import UserService from "../../services/user.service";
-import Service from "../../services/zapiszto"
-import EventBus from "../../common/EventBus";
 import TestForm from '../../components/zapiszto/testForm'
+import ShowBodyParams from "./showBodyParams";
 
 type Props = {};
+type State = {};
 
-type State = {
-  content: string;
-  responseData: any[];
-}
-
-export default class BoardUser extends Component<Props, State> {
+export default class BodyParams extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-
-    this.state = {
-      content: "",
-      responseData: []
-    };
+    this.state = {};
   }
-
-  componentDidMount() {
-    UserService.getUserBoard().then(
-      response => {
-        this.setState({
-          content: response.data
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString()
-        });
-
-        if (error.response && error.response.status === 401) {
-          EventBus.dispatch("logout");
-        }
-      }
-    );
-Service.getActualBodyParams()
-      //Service.getTestData()
-      .then(
-        (response) => {
-            this.setState({
-                responseData: response.data,
-            });
-        },
-        (error) => {
-            console.log("jakis błąd", error)
-        }
-      )
-  }
-  
-
   render() {
     return (
       <div className="container">
-        <header className="jumbotron">
-          <h3>{this.state.content}</h3>
-          <ul>
-            {this.state.responseData.map((item, index) => (
-              <li key={index}>
-                <strong>Kolumna 1:</strong> {item.kolumna_1},{" "}
-                <strong>Kolumna 2:</strong> {item.kolumna_2},{" "}
-                <strong>userId:</strong> {item.userId}
-              </li>
-            ))}
-          </ul>
           <div className="sa">
-          <TestForm />
+            <ShowBodyParams />
+          </div>
+          <div className="sa">
+            <TestForm />
+          </div>
       </div>
-        </header>
-      </div>
-      
     );
   }
 }
---
+++ frontend/src/components/zapiszto/showBodyParams.tsx
@@ -0,0 +1,56 @@
+import { Component } from "react";
+import Service from "../../services/zapiszto"
+
+
+type BodyParamsItem = {
+    kolumna_1: string;
+    kolumna_2: string;
+    userId: number;
+  };
+  
+  type Props = {};
+  type State = {
+    bodyParams: BodyParamsItem[];
+  };
+
+export default class ShowBodyParams extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            bodyParams: []
+        };
+    }
+
+    componentDidMount() {
+        Service.getActualBodyParams().then(
+                (response) => {
+                    this.setState({
+                        bodyParams: response.data,
+                    });
+                },
+                (error) => {
+                    console.log("jakis błąd", error)
+                }
+            )
+    }
+    render() {
+        return (
+            <div className="container">
+                <header className="jumbotron">
+                    <ul>
+                        {this.state.bodyParams.map((item: BodyParamsItem, index: number) => (
+                            <li key={index}>
+                                <strong>Kolumna 1:</strong> {item.kolumna_1},{" "}
+                                <strong>Kolumna 2:</strong> {item.kolumna_2},{" "}
+                                <strong>userId:</strong> {item.userId}
+                            </li>
+                        ))}
+                    </ul>
+                </header>
+            </div>
+
+        );
+    }
+
+}
