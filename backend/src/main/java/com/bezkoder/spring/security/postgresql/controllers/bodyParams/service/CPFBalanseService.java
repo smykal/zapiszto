8f2fdd596a3dcf82c9ff7a271c6498857a7c878e
Mateusz <smykal@engineer.com>
2024-04-04 20:57:12 +0200

    wip

--- backend/src/main/java/com/bezkoder/spring/security/postgresql/controllers/bodyParams/service/CPFBalanseService.java
+++ backend/src/main/java/com/bezkoder/spring/security/postgresql/controllers/bodyParams/service/CPFBalanseService.java
@@ -11,6 +11,15 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CPFBalanseService {
 
+  private final int carbohydrateMin = 5;
+  private final int carbohydrateMax = 5;
+
+  private final double proteinMin = 1.6;
+  private final double proteinMax = 2.2;
+
+  private final double fatMin = 0.5;
+  private final double fatMax = 1.5;
+
   @Autowired
   private CPFBalanseRepository cpfBalanseRepository;
 }
