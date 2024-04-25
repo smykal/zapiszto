038bebdd7ba8c6c91510e6fb9b157eb1f71be8b3
Mateusz <smykal@engineer.com>
2024-04-25 17:52:09 +0200

    edit project path

--- backend/src/main/java/com/zapiszto/payload/response/MessageResponse.java
+++ backend/src/main/java/com/zapiszto/payload/response/MessageResponse.java
@@ -1,17 +1,14 @@
 package com.zapiszto.payload.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class MessageResponse {
   private String message;
 
   public MessageResponse(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
     this.message = message;
   }
 }
