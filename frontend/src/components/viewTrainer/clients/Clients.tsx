cb187c49b3a518216d65c47c847871f699bb008c
Mateusz <smykal@engineer.com>
2024-05-29 22:26:07 +0200

    begining of trainer view

---
+++ frontend/src/components/viewTrainer/clients/Clients.tsx
@@ -0,0 +1,11 @@
+const Clients = () => {
+    return (
+      <div>
+        <h1>Clients</h1>
+        <p>Here you can manage your clients.</p>
+      </div>
+    );
+  };
+  
+  export default Clients;
+  
