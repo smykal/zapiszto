504c1a9016064ddcec286b0b746c3155dd61a9c2
Mateusz <smykal@engineer.com>
2024-08-03 23:29:00 +0200

    add JSON to translate dictionary value

--- backend/src/main/java/com/zapiszto/controllers/dictionaries/dictQuantityType/dictQuantityTypeBasic/entity/DictQuantityTypeBasicEntity.java
+++ backend/src/main/java/com/zapiszto/controllers/dictionaries/dictQuantityType/dictQuantityTypeBasic/entity/DictQuantityTypeBasicEntity.java
@@ -1,11 +1,15 @@
 package com.zapiszto.controllers.dictionaries.dictQuantityType.dictQuantityTypeBasic.entity;
 
+
+import com.zapiszto.controllers.dictionaries.dictLanguages.options.Languages;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,6 +18,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "dict_quantity_type_basic")
@@ -29,8 +35,9 @@ public class DictQuantityTypeBasicEntity {
   @Column(name = "id")
   UUID id;
 
+  @JdbcTypeCode(SqlTypes.JSON)
   @Column(name = "name")
-  String name;
+  HashMap<String, String> name;
 
   @Column(name = "shortcut")
   String shortcut;
